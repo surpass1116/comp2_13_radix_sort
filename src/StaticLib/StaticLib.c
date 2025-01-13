1bd41925594220a51128048a70a38ff7513bdca4
surpass1116 <greenrikun16@gmail.com>
2025-01-13 15:10:32 +0900

    基数ソートを実装

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -2,6 +2,7 @@
 #include "Windows.h"                    // Windows API の機能定義
 
 #include "../include/lib_func.h"
+#include<stdlib.h>
 
 
 // 基数ソート(引数が不適切であればfalseを返す)
@@ -11,5 +12,38 @@
 bool radix_sort(item* begin, const item* end, int radix)
 {
 	// ToDo: 基数ソートを実装する
-	return false;
+	if (begin == NULL || end == NULL || radix <= 0 || begin > end)return false;
+
+	int num = (end - begin);
+
+	item* a = malloc(sizeof(item) * num);
+	int* rad = malloc(sizeof(int) * num);
+	
+
+	int m = 1;
+	item* n = begin;
+
+	while (m <= num) {	//ここのnumは本来radix
+		for (int i = 0; i < num; i++) {
+			rad[i] = ((n + i)->key / m) % 10;
+		}
+		int k = 0;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < num; j++) {
+				if (rad[j] == i) {
+					a[k] = *(n + j);
+					k++;
+				}
+			}
+		}
+		for (int i = 0; i < num; i++) {
+			*(n + i) = a[i];
+		}
+		m *= 10;
+	}
+
+	free(a);
+	free(rad);
+
+	return true;
 }
